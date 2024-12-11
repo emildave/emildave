f3bfa73fb8c7d55d7347e691505b1daaca17b3e4
Emil Cabutotan <emildavec@gmail.com>
2024-12-10 18:25:33 -0700

    added pfp

--- app/components/Header.js
+++ app/components/Header.js
@@ -1,6 +1,11 @@
 export default function Header() {
   return (
     <header className="flex flex-col items-center py-6">
+      <img
+        src="https://github.com/emildave.png"
+        alt="Profile Picture"
+        className="w-24 h-24 rounded-full mb-4"
+      />
       <h1 className="text-4xl font-bold text-accent mb-4">Emil Cabutotan</h1>
       <nav>
         <ul className="flex flex-col space-y-4">
